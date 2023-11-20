c39405f1e0b243e23dc1d91f17bb9574fce95633
Andremxsrs <ronnyandres120@gmail.com>
2023-11-19 21:41:30 -0500

    Cambios en el home pharma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js" // add this line
+  ],
   theme: {
     extend: {},
+    
+    colors:{
+      azul:{
+        200: '#7688B5',
+        500:'#009EFA'
+      }
+    },
+    boxShadow:{
+      azulbs: '1px 8px 30px 0px rgba(0, 158, 250, 0.70)'
+    },
+
   },
   plugins: [
-    require('flowbite/plugin'),
+    require('flowbite/plugin')
   ],
 }
